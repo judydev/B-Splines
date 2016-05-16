39a6079e46ba797a087e12027aeeeb50be35aae4
Di (Judy) Zhu <judysome@gmail.com>
2016-05-16 15:11:22 -0600

    Update basisfunc_NURBS.m

--- basisfunc_NURBS.m
+++ basisfunc_NURBS.m
@@ -3,11 +3,11 @@ function R_ik = basisfunc_NURBS( N, K, T, w )
 %
 %   INPUT:
 %   N - number of control points
-%       e.g. if there are 5 control points, N = 5.
+%       e.g. if there are 5 control points, N = 5. (note that matlab is not zero-indexed)
 %   K - order (degree + 1) 
 %       e.g. for quadratic B-Splines, degree = 2; order K = 2 + 1 = 3.
 %   T - knot vector, a nondecreasing sequence from t_0 to t_(N+K-1)
-%       e.g. for N = 4, K = 3, a knot vector can be:
+%       e.g. for N = 5, K = 3, a knot vector can be:
 %           [t0 t1 t2 t3 t4 t5 t6 t7]  -> parameters
 %           [ 0  0  0  1  2  3  3  3]  -> nonuniform
 %   w - weight factors, w_i is the weight(>=0) for each control point
